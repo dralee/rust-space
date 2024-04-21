ca9612892ec0da9f4d7ad848d97b56ebe28ffbfb
dralee <zhzl_s@126.com>
2024-04-21 18:10:47 +0800

    feat: query

--- draleewebmvc/draleewebmvc/src/request.rs
+++ draleewebmvc/draleewebmvc/src/request.rs
@@ -2,7 +2,7 @@
  * 请求对象
  * 2024.04.20 by dralee
  */
-use std::{collections::HashMap};
+use std::{collections::{btree_map::Keys, HashMap}, rc::Rc};
 
 ///
 /// http请求实体
@@ -12,7 +12,8 @@ pub struct Request {
 	pub host: String,
 	pub url: String,
 	pub path: String,
-	pub query: String,
+	pub query_string: String,
+	pub query: HashMap<String, Vec<String>>,
 	pub content_type: String,
 	pub user_agent: String,
 	pub headers: HashMap<String, String>,
@@ -46,15 +47,36 @@ impl Request {
 		Request::resolve(lines, buffer)
 	}
 
+	fn resolve_query(query_string: &str) -> HashMap<String, Vec<String>> {
+		let mut query = HashMap::new();
+		query_string.split('&').for_each(|s|{
+			let items:Vec<&str> = s.split('=').collect();
+			let mut value:Rc<String> = Rc::new(String::from(""));
+			let key = items[0].to_string();
+			if items.len() >= 1 {				
+				if items.len() > 2 {
+					value = Rc::new(items[1..].join("="));
+				} else {
+					value = Rc::new(items[1].to_string());
+				}
+			}
+
+			query.entry(key).and_modify(|v:&mut Vec<String>|v.push(Rc::clone(&value).to_string())).or_insert(vec![Rc::clone(&value).to_string()]);			
+			//query.insert(key, value_set);
+		});
+
+		query
+	}
+
 	fn resolve(lines: Vec<String>, buffer: Vec<u8>) -> Request {
 		let head_line_items:Vec<&str> = lines[0].split(' ').collect();
 		let url = head_line_items[1];
 		let method = head_line_items[0];
 		let mut path = String::new();
-		let mut query = String::new();
+		let mut query_string = String::new();
 		if let Some(index) = url.find('?') {
 			path = String::from(&url[..index]);
-			query = String::from(&url[index+1..]);
+			query_string = String::from(&url[index+1..]);
 		}
 
 		let mut headers = HashMap::new();
@@ -95,10 +117,13 @@ impl Request {
 			}
 		}
 
+		let query = Request::resolve_query(&query_string);
+
 		Request {
 			headers,
 			path,
 			method: method.to_string(),
+			query_string,
 			query,
 			body: buffer,
 			host: host,
